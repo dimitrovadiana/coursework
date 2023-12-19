94d6fc81a72938d02f9459146b31204091459578
Ibraim Asanov <iasanov@nemetschek.bg>
2023-12-19 16:46:46 +0200

    Initial commit

---
+++ src/test/java/base/TestUtil.java
@@ -0,0 +1,24 @@
+package base;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestUtil {
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupDriverAndOpenTargetURL(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+    }
+
+    @AfterMethod
+    public void tearDown(){
+        driver.quit();
+    }
+
+}
